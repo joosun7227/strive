c58eee9f78ef9e269c65085fad6862b57492a999
joosun7227 <96054361+joosun7227@users.noreply.github.com>
2025-09-20 19:29:27 +0900

    concole

---
+++ javascript/window/script1.js
@@ -0,0 +1,20 @@
+let val;
+
+val = window.outerHeight;
+val = window.outerHeight;
+
+val = window.innerHeight;
+val = window.innerWidth;
+
+val = window.scrollY;
+val = window.scrollX;
+
+val = window.location; //현재 페이지의 정보
+// window.location.href = 'https://naver.com';
+
+val = window.history.back();
+val = window.history.forward()
+
+// window.navigator();
+
+console.log(val);
