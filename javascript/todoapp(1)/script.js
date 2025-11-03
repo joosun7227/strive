7b1e0d17fc69168874f855e5e7b34a0b876b61a9
jusun <joosun7227@gmail.com>
2025-11-03 11:10:26 +0900

    todoapp-2

---
+++ javascript/todoapp(1)/script.js
@@ -0,0 +1,10 @@
+const list = document.getElementById('list');
+const createBtn = document.getElementById('create-btn');
+
+let todos = [];
+
+createBtn.addEventListener('click', createNewTodo);
+
+function createNewTodo() {
+
+}
